b06f3d12fa588020ff8086bd22a33f1c3ca2bd01
Predrag Vila <>
2022-04-03 14:44:48 +0500

    initial commit

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci debitis earum illo facere mollitia ut quibusdam! Eveniet, dolore. Impedit ratione fugiat eaque quaerat laborum! Suscipit sed nulla quod facere. Minus?',
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci debitis earum illo facere mollitia ut quibusdam! Eveniet, dolore. Impedit ratione fugiat eaque quaerat laborum! Suscipit sed nulla quod facere. Minus?',
+  },
+  {
+    id: 3,
+    rating: 7,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci debitis earum illo facere mollitia ut quibusdam! Eveniet, dolore. Impedit ratione fugiat eaque quaerat laborum! Suscipit sed nulla quod facere. Minus?',
+  },
+];
+
+export default FeedbackData;
